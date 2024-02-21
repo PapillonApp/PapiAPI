33b8fde7667a4d239d6e3852d3f95699ece622c5
Olivier <olivier@thff.fr>
2024-02-21 13:00:51 +0100

    Ajout de contenu de test dans les json + renvoie des json

--- api.py
+++ api.py
@@ -1,8 +1,29 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify
 
-api = Blueprint('api', "PapiApi")
+api = Blueprint('api', __name__)
+
 
 @api.route("/")
 def root():
-    return "API Papillon"
+        with open("data/api.json", "r") as file:
+            apiFile = json.load(file)
+        return jsonify(apiFile)
+    
+@api.route("/staff")
+def staff():
+    with open("data/staff.json", "r") as file:
+        staffFile = json.load(file)
+    return jsonify(staffFile)
 
+@api.route("/warning")
+def warning():
+    with open("data/warning.json", "r") as file:
+        warningFile = json.load(file)
+    return jsonify(warningFile)
+
+@api.route("/disclaimerIE")
+def disclaimerIE():
+    with open("data/disclaimerIE.json", "r") as file:
+        disclaimerIEFile = json.load(file)
+    return jsonify(disclaimerIEFile)
