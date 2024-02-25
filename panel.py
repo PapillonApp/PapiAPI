ea75e62f18fce9bdeed1b1a510fb11738ad337ef
Tryon <68423470+tryon-dev@users.noreply.github.com>
2024-02-25 21:32:41 +0100

    First Commit for me

--- panel.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, send_from_directory
-
-panel = Blueprint('panel', "PapiApi")
-
-@panel.route("/panel")
-def root():
-    return send_from_directory("panel", "index.html")
-
